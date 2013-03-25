7906cec7b0f2295698833184f935292843d3e6f5
Roel van Uden <roel.van.uden@hotmail.com>
2013-03-25 11:29:08 +0100

    KM/Batoto: Support for alphabetic chapter suffix.

--- MangaRack.Provider.KissManga/Extension/ExtensionForString.cs
+++ MangaRack.Provider.KissManga/Extension/ExtensionForString.cs
@@ -10,6 +10,29 @@ namespace MangaRack.Provider.KissManga {
 	/// </summary>
 	public static class ExtensionForString {
 		/// <summary>
+		/// Convert an alphabetic suffix to a numeric suffix.
+		/// </summary>
+		/// <param name="Value">The value.</param>
+		public static string AlphabeticToNumeric(this string Value) {
+			// Retrieve the alphabetic value.
+			char Alphabetic = Value.ToLowerInvariant()[Value.Length - 1];
+			// Check if the presumed alphabetic value is actually alphabetic.
+			if (!char.IsDigit(Alphabetic)) {
+				// Remove the alphabetic value.
+				Value = Value.Substring(0, Value.Length - 1);
+				// Check if the value does not values behind the digit.
+				if (Value.IndexOf('.') == -1) {
+					// Append the digit separator.
+					Value += '.';
+				}
+				// Convert the alphabetic value to numeric and append it.
+				Value += ((int) Alphabetic - 96).ToString();
+			}
+			// Return the value.
+			return Value;
+		}
+
+		/// <summary>
 		/// Remove characters before and including the first occurrence of the subject.
 		/// </summary>
 		/// <param name="Value">The value.</param>
