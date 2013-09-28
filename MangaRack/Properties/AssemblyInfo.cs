4abb89bcd6227b3c3726766ba4df21fe4e5e893b
Roel van Uden <roel.van.uden@hotmail.com>
2013-09-28 18:23:21 +0200

    Release for binary (fixed binary merging).

--- MangaRack/Properties/AssemblyInfo.cs
+++ MangaRack/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 ﻿[assembly: System.Reflection.AssemblyCopyright("(C) Roel van Uden. All rights reserved.")]
 [assembly: System.Reflection.AssemblyVersion("2.0.*")]
 
-// Embedding
-[assembly: System.Reflection.Obfuscation(Feature = "embed AForge.dll", Exclude = false)]
-[assembly: System.Reflection.Obfuscation(Feature = "embed AForge.Imaging.dll", Exclude = false)]
-[assembly: System.Reflection.Obfuscation(Feature = "embed AForge.Math.dll", Exclude = false)]
-[assembly: System.Reflection.Obfuscation(Feature = "embed CommandLine.dll", Exclude = false)]
-[assembly: System.Reflection.Obfuscation(Feature = "embed ICSharpCode.SharpZipLib.dll", Exclude = false)]
+// Switches
+[assembly: System.Reflection.Obfuscation(Feature = "ilmerge custom parameters: /allowDup", Exclude = false)]
 
 // Merging
+[assembly: System.Reflection.Obfuscation(Feature = "merge with AForge.dll", Exclude = false)]
+[assembly: System.Reflection.Obfuscation(Feature = "merge with AForge.Imaging.dll", Exclude = false)]
+[assembly: System.Reflection.Obfuscation(Feature = "merge with AForge.Math.dll", Exclude = false)]
+[assembly: System.Reflection.Obfuscation(Feature = "merge with CommandLine.dll", Exclude = false)]
 [assembly: System.Reflection.Obfuscation(Feature = "merge with HtmlAgilityPack-PCL.dll", Exclude = false)]
+[assembly: System.Reflection.Obfuscation(Feature = "merge with ICSharpCode.SharpZipLib.dll", Exclude = false)]
 [assembly: System.Reflection.Obfuscation(Feature = "merge with MangaRack.Core.dll", Exclude = false)]
 [assembly: System.Reflection.Obfuscation(Feature = "merge with MangaRack.Provider.dll", Exclude = false)]
 [assembly: System.Reflection.Obfuscation(Feature = "merge with MangaRack.Provider.Batoto.dll", Exclude = false)]
